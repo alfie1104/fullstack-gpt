5c1e6a84a3c9e710008173bd1b9f1372d42df650
peter_alfie <alfie1104@gmail.com>
2024-01-19 07:26:48 +0900

    feat : Persist chat data
    - Persist chat data by using streamlit.session_state

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -2,12 +2,17 @@ import time
 import streamlit as st
 st.title("DocumentGPT")
 
-messages = []
+if "messages" not in st.session_state:
+    st.session_state["messages"] = []
 
-def send_message(message, role):
+def send_message(message, role, save=True):
     with st.chat_message(role):
         st.write(message)
-        messages.append({"message":message,"role":role})
+    if save:
+        st.session_state["messages"].append({"message":message,"role":role})
+
+for message in st.session_state["messages"]:
+    send_message(message["message"], message["role"], save=False)
 
 message = st.chat_input("Send a message to the ai")
 
@@ -15,3 +20,6 @@ if message:
     send_message(message, "human")
     time.sleep(2)
     send_message(f"You said: {message}","ai")
+
+    with st.sidebar:
+        st.write(st.session_state)
