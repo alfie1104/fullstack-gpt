d4b39a85c257e4db57460bfdd61a848fe240be06
peter_alfie <alfie1104@gmail.com>
2024-01-24 08:27:52 +0900

    feat : Setting up for private gpt
    - create cache folders for private
    - copy the code of document gpt and paste it to private gpt file
    - change the order of the pages(private gpt <-> quiz gpt)

--- pages/02_PrivateGPT.py
+++ pages/02_PrivateGPT.py
@@ -11,7 +11,7 @@ from langchain.callbacks.base import BaseCallbackHandler
 import os
 
 st.set_page_config(
-    page_title="DocumentGPT",
+    page_title="PrivateGPT",
     page_icon="📃",
 )
 
@@ -46,7 +46,7 @@ llm = ChatOpenAI(
 @st.cache_data(show_spinner="Embedding file...")
 def embed_file(file):
     file_content = file.read()
-    file_path = f"./.cache/files/{file.name}"
+    file_path = f"./.cache/private_files/{file.name}"
     
     dir_path = os.path.dirname(file_path)
     if not os.path.exists(dir_path):
@@ -55,7 +55,7 @@ def embed_file(file):
     with open(file_path,"wb") as f:
         f.write(file_content)
 
-    cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
+    cache_dir = LocalFileStore(f"./.cache/private_embeddings/{file.name}")
     splitter = CharacterTextSplitter.from_tiktoken_encoder(
         separator="\n",
         chunk_size=600,
