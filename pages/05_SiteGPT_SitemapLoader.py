bf0c7537a42464ee6a40891cea4cc34e6cb27334
peter_alfie <alfie1104@gmail.com>
2024-02-07 08:58:47 +0900

    feat : Map Re Rank Chain - part1
    - Map Re Rank Chain
     retriever로 부터 받은 document를 살펴보고 그 document들을 llm에게 전달하면서, 해당 document들로만 사용자의 question에 답변해달라고 요청함
     그리고 나서 각 document를 이용하여 답변들이 생성되면, llm에게 답변의 유용한 정도를 평가해달라고 요청함
    
     모든 답변과 점수는 또 다른 prompt에게 입력되고, 그 prompt는 다시 llm에 전달되어
     '주어진 답변들 중 가장 높은 점수를 갖고 가장 최근에 작성된 것을 선택해줘'라고 요청
    
     따라서 2개의 chain이 필요함
     1) 답변 생성 및 채점 담당
     2) 점수가 제일 높고 가장 최신정보를 담고 있는 답변을 고르는 chain
    
     본 커밋은 1번 체인에 대한 커밋임

--- pages/05_SiteGPT_SitemapLoader.py
+++ pages/05_SiteGPT_SitemapLoader.py
@@ -2,6 +2,9 @@ from langchain.document_loaders import SitemapLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores.faiss import FAISS
 from langchain.embeddings import OpenAIEmbeddings
+from langchain.schema.runnable import RunnablePassthrough, RunnableLambda
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
 import streamlit as st
 
 import asyncio
@@ -16,6 +19,54 @@ asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy()) # window
 #  - 속도르 더 줄이려면 SitemapLoader(url).requests_per_second 함수를 통해 조절할 수 있음
 #  - SitemapLoader는 page로부터 모든 text를 추출하고 html을 제거하기 위해 내부적으로 beautiful soup(soup is just bunch of html) 를 사용하고 있음.
 #  - parsing_function 속성을 설정하면 beautiful soup의 동작을 조정할 수 있음
+
+llm = ChatOpenAI(
+    temperature=0.1
+)
+
+answers_prompt = ChatPromptTemplate.from_template("""
+    Using ONLY the following context answer the user's question. If you can't just say you don't know, don't make anything up.
+                                                  
+    Then, give a score to the answer between 0 and 5.
+    If the answer answers the user question the score should be high, else it should be low.
+    Make sure to always include the answer's score even if it's 0.
+    Context: {context}
+                                                  
+    Examples:
+                                                  
+    Question: How far away is the moon?
+    Answer: The moon is 384,400 km away.
+    Score: 5
+                                                  
+    Question: How far away is the sun?
+    Answer: I don't know
+    Score: 0
+                                                  
+    Your turn!
+    Question: {question}
+""")
+
+def get_answers(inputs):
+    docs = inputs["docs"]
+    question = inputs["question"]
+    answers_chain = answers_prompt | llm
+
+    # answers = []
+    # for doc in docs:
+    #     result = answers_chain.invoke({
+    #         "question":question,
+    #         "context":doc.page_content
+    #     })
+    #     answers.append(result.content)
+    return [
+        {
+            "answer" : answers_chain.invoke({
+                    "question":question,"context":doc.page_content
+                }).content,
+            "source": doc.metadata["source"],
+            "date" : doc.metadata["lastmod"],
+        } for doc in docs
+    ]
 
 def parse_page(soup):
     # beautiful soup가 생성하는 soup object를 이용해서 필요한 데이터만 추출
@@ -88,6 +139,21 @@ if url:
     else:
         # docs = load_website(url)
         retriever = load_website(url) # 기존에는 load_website가 document를 그대로 반환했지만 이제 retriever로 반환
-        docs = retriever.invoke("What is the price of GPT-4 Turbo")
+        # Map Re Rank Chain
+        # retriever로 부터 받은 document를 살펴보고 그 document들을 llm에게 전달하면서, 해당 document들로만 사용자의 question에 답변해달라고 요청함
+        # 그리고 나서 각 document를 이용하여 답변들이 생성되면, llm에게 답변의 유용한 정도를 평가해달라고 요청함
+        # 모든 답변과 점수는 또 다른 prompt에게 입력되고, 그 prompt는 다시 llm에 전달되어
+        # '주어진 답변들 중 가장 높은 점수를 갖고 가장 최근에 작성된 것을 선택해줘'라고 요청
+        # 따라서 2개의 chain이 필요함. 1) 답변 생성 및 채점 담당 / 2) 점수가 제일 높고 가장 최신정보를 담고 있는 답변을 고르는 chain
 
-        st.write(docs)
+        # question이 retriever로 전달되어서 retriever는 docs를 반환하고,
+        # 동일한 question이 다시 question 파라미터에 할당됨
+        # retriever로부터 반환받은 docs와 question 파라미터를 get_answer 함수의 입력으로 전달하여
+        # get_answers는 각 doc마다 답변 및 점수를 반환함
+        chain = {
+            "docs":retriever, 
+            "question":RunnablePassthrough()
+        } | RunnableLambda(get_answers) # docs와 question이 get_answers함수의 입력으로 전달됨
+        # | RunnableLambda(choose_answer)
+
+        chain.invoke("What is the pricing of GPT-4 Turbo with vision.")
