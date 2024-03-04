a286224456b7169b4aa1f8af1ae6af1df1ac4c36
peter_alfie <alfie1104@gmail.com>
2024-03-05 08:54:22 +0900

    feat : apply Pinecone vector store at main.py

--- main.py
+++ main.py
@@ -2,30 +2,48 @@ from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from pydantic import BaseModel, Field
 from typing import Any
+from dotenv import load_dotenv
 import os
 
+import pinecone
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores.pinecone import Pinecone
+
+load_dotenv() # FastAPI에서 .env의 모든 변수를 읽어들이도록 하기 위해 load_dotenv 함수 호출. Jupyter notebook과 달리 fastapi에서는 이걸 호출하지 않으면 os.getenv()에서 .env파일에 적힌 키를 입력해도 값을 얻을 수 없음
+
+# Pinecone은 cloud vector DB임 (FAISS같은 vector store를 cloud 환경에서 제공)
+pinecone.init(
+    apy_key=os.getenv("PINECONE_API_KEY"),
+    environment="gcp-starter"
+)
+
+
+embeddings = OpenAIEmbeddings()
+vector_store = Pinecone.from_existing_index("recipes", embeddings)
+
+
 app = FastAPI(
-    title="Dante Quote Giver",
-    description="Get a real quote said by Dante himself.",
+    title="Chef Dante. The best provider of Indian Recipes in the world.",
+    description="Give Chef Dante the name of an ingredient and it will give you multiple recipes to use that ingredient on in return.",
     servers=[
-        {"url":"https://registry-mount-pd-calls.trycloudflare.com"}
+        {"url":"https://quarters-monthly-increase-bras.trycloudflare.com "}
     ]
 )
 
-class Quote(BaseModel):
-    quote: str = Field(description="The quote that Dante said.")
-    year: int = Field(description="The year when Dante said the quote.")
+class Document(BaseModel):
+    page_content: str
 
-@app.get("/quote", summary="Returns a random quote by Danten"
-         , description="Upon receiving a GET request this endpoint will return a real quiote said by Dante himself."
-         , response_description="A Quote object that contains the quote said by Dante and the date when the quote was said."
-         , response_model = Quote)
-def get_quote(request: Request):
-    print(request.headers)
-    return {
-        "quote":"Life is short so eat it all.",
-        "year":1995
-    }
+@app.get("/recipes", summary="Returns a list of recipes."
+         , description="Upon receiving an ingredient, this endpoint will return a list of recipes that contain that ingredient."
+         , response_description="A Document object that contains the recipe and preparation instructions"
+         , response_model = list[Document]
+         , openapi_extra={
+             "x-openai-isConsequential": False
+         })
+def get_recipe(ingredient:str):
+    # /recipes?ingredient=xxxx 로 입력되는 쿼리 파라미터를 ingredient에 할당
+    docs = vector_store.similarity_search(ingredient)
+    return docs
 
 user_token_db = {
     "ABCDEF":"test_token"
@@ -40,7 +58,7 @@ def handle_authorize(client_id:str, redirect_uri:str, state:str):
         </head>
         <body>
             <h1>Log Into Dante</h1>
-            <a href="{redirect_uri}?code=ABCDEF&state={state}">Authorize Dante GPT</a>
+            <a href="{redirect_uri}?code=ABCDEF&state={state}">Authorize Dante</a>
         </body>
     </html>
     """
